e466661cf2e4dfe8b1e466a3cb5034eeeabc00b7
Todor Tsonkov <tabledott@gmail.com>
2026-02-12 18:05:16 +0200

    Search issues fixed

---
+++ firebase-config.template.js
@@ -0,0 +1,10 @@
+// Copy this file to firebase-config.js and fill in your Firebase project settings
+// https://console.firebase.google.com/ -> Project Settings -> Your apps
+window.firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
